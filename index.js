cc1c6acdec550b8f349382516d8fa580101bd05d
PhoenixClix <mfirstborn@gmail.com>
2021-04-16 13:18:18 +0300

    added How to Delete a Git Branch Both Locally and Remotely

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log('this is a javascript file');
 console.table({'name'})
+
+// How to Delete a Git Branch Both Locally and Remotely 
+
+// Deleting a branch LOCALLY
+// Delete a branch with git branch -d <branch> . The -d option will delete the branch only if it has already been pushed and merged with the remote branch. Use -D instead if you want to force the branch to be deleted, even if it hasn't been pushed or merged yet. The branch is now deleted locally.
+
+// console.table({'git branch -d <branch>'}) 
