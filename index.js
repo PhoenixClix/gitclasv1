b41c19bf2d194a4c7a7808aed96ee7306349fc50
PhoenixClix <mfirstborn@gmail.com>
2021-04-16 13:24:54 +0300

    updated with How to Undo the 'init' command on Git

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ console.table({'name'})
 // Delete a branch with git branch -d <branch> . The -d option will delete the branch only if it has already been pushed and merged with the remote branch. Use -D instead if you want to force the branch to be deleted, even if it hasn't been pushed or merged yet. The branch is now deleted locally.
 
 // console.table({'git branch -d <branch>'}) 
+
+
+// How to Undo the 'init' command on Git? - Appuals.com
+
+// In order to do that:
+// Press “Ctrl” + “Alt” + “T” to open the terminal. Opening the Terminal.
+// Type in the following command and press “Enter”. rm -rf .git
+// This will delete the entire git repository and undo the changes made by the init command.
+
+
